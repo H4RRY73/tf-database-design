725bbc14c16f67da214544b42db48168c21aeec7
Mily27 <107314451+Mily27@users.noreply.github.com>
2023-11-24 08:20:50 -0500

    Update u202116078.js

--- universal-friends/non-relational-queries/u202116078.js
+++ universal-friends/non-relational-queries/u202116078.js
@@ -1,7 +1,11 @@
 // Obtener usuarios que comparten ciertos intereses
 db.usuarios.find({
     intereses: {
-        $in: ["interes1", "interes2"]
+        $in: ["Programación", "Fotografía"]
     }
 })
-// falta unnaaa
+// Obtener la puntuación máxima y mínima para un docente específico 
+db.valoraciones.aggregate([
+  { $match: { codigo_docente_valorado: 3 } },
+  { $group: { _id: null, max_puntuacion: { $max: "$puntuacion" }, min_puntuacion: { $min: "$puntuacion" } } }
+])
